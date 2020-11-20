53baaa48be2ca4c02ad1f182a6d9dfa6d10eba6b
Maria Trujillo <mptruj015@gmail.com>
2020-11-20 14:56:43 +0100

    added comments to pieChart

--- src/components/pie_chart/PieChart.js
+++ src/components/pie_chart/PieChart.js
@@ -2,12 +2,13 @@ import React, { useLayoutEffect, useRef } from 'react';
 import * as am4core from '@amcharts/amcharts4/core';
 import * as am4charts from '@amcharts/amcharts4/charts';
 
+// data passed in as props from PieChartContainer
 const PieChart = data => {
   const chart = useRef(null);
   useLayoutEffect(() => {
     //   creates instance of chart
     let pie_chart = am4core.create('chartdiv', am4charts.PieChart);
-    //   sets data
+    //   sets data; needs to be accessed as data.data
     pie_chart.data = data.data;
 
     //   creates a new series
@@ -15,6 +16,8 @@ const PieChart = data => {
     // sets up series
     pieSeries.dataFields.value = 'count';
     pieSeries.dataFields.category = 'type_of_force';
+
+    // equivalent to ComponentDidMount; needed as per amCharts docs
     chart.current = pie_chart;
 
     // set legend
@@ -32,11 +35,13 @@ const PieChart = data => {
     as2.properties.textDecoration = 'line-through';
     as2.properties.fill = am4core.color('#000');
 
+    // equivalent to ComponentDidUnmount; needed as per amChart docs
     return () => {
       pie_chart.dispose();
     };
   }, []);
 
+  // div that actually holds the chart
   return <div id="chartdiv" style={{ width: '100%', height: '60vh' }}></div>;
 };
 export default PieChart;
